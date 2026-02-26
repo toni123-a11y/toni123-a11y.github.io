f970182030e1793a7297b286f840e46e134195ae
inooo._3 <75324781+toni123-a11y@users.noreply.github.com>
2026-02-26 16:55:43 +0700

    Update gate in.py

--- parking os/gate in.py
+++ parking os/gate in.py
@@ -1,9 +1,19 @@
 import os
+from datetime import datetime
+#waktu
+jam_masuk = datetime.now().strftime("%H:%M:%S")
 #cetak struk
+def cetak_struk(jam_masuk):
+    with open("struk_parkir.txt", "w") as file:
+        file.write("\n" + "="*20)
+        file.write("\n   STRUK PARKIR   ")
+        file.write("\n" + "="*20)
+        file.write(f"\nJam Masuk: {jam_masuk}")
+        file.write(f"\nTerima kasih telah menggunakan layanan parkir kami!")
 
 #main
 print("=" * 20)
-print("parking os")
+print("Secure parking os")
 print("=" * 20)
 print("Selamat datang")
 print("1. Parkir Mobil")
@@ -13,12 +23,13 @@ print("3. Keluar")
 pilihan = input("Masukan pilihan (1/2/3): ")
 
 if pilihan == "1":
-    jam_masuk = input("Masukan jam masuk (HH:MM): ")
+    jam_masuk = datetime.now().strftime("%H:%M:%S")
     print(f"Mobil parkir pada jam {jam_masuk}.")
+    cetak_struk(jam_masuk)
 
 elif pilihan == "2":
-    jam_masuk = input("Masukan jam masuk (HH:MM): ")
     print(f"Motor parkir pada jam {jam_masuk}.")
+    cetak_struk(jam_masuk)
 
 elif pilihan == "3":
     print("Selamat datang di mall bg juction")
